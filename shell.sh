680d48d5f65944f617169585d99824675ebf3d7e
WSMathias <wsmsamuel@gmail.com>
2017-12-01 00:19:49 +0530

    custom RT

--- shell.sh
+++ shell.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-CWD="/shared/httpd"
-docker-compose exec --user devilbox php env TERM=xterm /bin/sh -c "cd ${CWD}; exec bash -l"
+CWD="/shared/httpd/reclaimtemples"
+docker-compose exec --user devilbox php env TERM=xterm /bin/sh -c "cd ${CWD} ; exec bash -l"
